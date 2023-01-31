7fa8459090fba10bde6e1f9a4858b7dca6c14da1
Mohd Jamil <mohammedjameel2412@gmail.com>
2023-01-31 15:49:51 +0000

    three-counter

---
+++ three-counter/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Counter app using State/hooks</h1>
+      </header>
+      <h2>Current value of count is {count}</h2>
+      <button onClick={() => setCount(0)}>Reset Counter</button>
+      <button onClick={() => setCount(count + 1)}>Incease Counter</button>
+      <button onClick={() => setCount(count - 1)}>Decrease Counter</button>
+    </div>
+  );
+}
+
+export default App;
+ReactDOM.render(<App/>,document.getElementById("root"));
